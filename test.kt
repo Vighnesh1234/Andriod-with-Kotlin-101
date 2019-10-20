b36b430468069abf8aca15443429a501865d0048
VIGHNESH SINGH THAKUR <40726015+Vighnesh1234@users.noreply.github.com>
2019-10-19 22:32:37 -0600

    Update test.kt

--- test.kt
+++ test.kt
@@ -8,4 +8,16 @@ fun main(){
     var U:String ="Wtf vig"
 var i:Int=100
     println("My name is ${i/4}")// String Interpolation
+    
+    
+    //if-else
+    val catLives=9
+    val cond=catLives <1
+    
+    if(cond)
+    println("cat is dead")
+    
+    else
+    println("The cat is still alive")
+    
 }
