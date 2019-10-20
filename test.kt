0a9b2158b9fbca24486e8cd63f5eae563db1630f
VIGHNESH SINGH THAKUR <40726015+Vighnesh1234@users.noreply.github.com>
2019-10-19 22:18:04 -0600

    Create test.kt
    Basics

---
+++ test.kt
@@ -0,0 +1,11 @@
+/**
+ * We declare a package-level function main which returns Unit and takes
+ * an Array of strings as a parameter. Note that semicolons are optional.
+ */
+fun main(){
+    println("Yeah")
+    
+    var U:String ="Wtf vig"
+var i:Int=100
+    println("My name is ${i/4}")// used for concatenating/using integers with String 
+}
